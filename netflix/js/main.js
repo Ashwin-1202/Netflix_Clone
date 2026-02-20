a32e8a58b405a812a019378411a52eeb52b42652
Ashwin V <ashwinv2023@gmail.com>
2026-02-20 22:52:54 +0530

    Initial commit

---
+++ netflix/js/main.js
@@ -0,0 +1,30 @@
+(function ($) {
+    "use strict";
+
+    $(document).ready(function () {
+
+        function default_home_carouselInit() {
+            $('.owl-carousel.trending_active').owlCarousel({
+                dots: false,
+                loop: false, /* FIXED: Set to false to stop at #1 */
+                rewind: true, /* Optional: If true, clicking prev at #1 jumps to #10. If false, it just stops. */
+                margin: 10,
+                stagePadding: 0,
+                nav: true,
+                navText: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+                autoplay: false,
+                smartSpeed: 500,
+                responsive: {
+                    0: { items: 2, margin: 5 },
+                    576: { items: 3 },
+                    768: { items: 4 },
+                    992: { items: 5 },
+                    1200: { items: 6 }
+                }
+            });
+        }
+        default_home_carouselInit();
+
+    });
+
+})(jQuery);
